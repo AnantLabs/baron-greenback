302fbe063c19a420bb5a0e0b3d25cbb74b5ae147
stuart <stuart@localhost>
2012-12-16 18:55:11 +0000

    First stab of new boostrap

--- test/com/googlecode/barongreenback/ShowAndTell.java
+++ test/com/googlecode/barongreenback/ShowAndTell.java
@@ -14,13 +14,14 @@ import java.util.Properties;
 import java.util.UUID;
 
 import static com.googlecode.barongreenback.persistence.lucene.LucenePersistence.luceneDirectory;
+import static com.googlecode.barongreenback.persistence.lucene.LucenePersistence.luceneTemporaryDirectory;
 import static com.googlecode.utterlyidle.RequestBuilder.post;
 import static com.googlecode.utterlyidle.ServerConfiguration.defaultConfiguration;
 
 public class ShowAndTell {
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
-        PersistenceUri.set(properties, luceneDirectory(new File("/dev/shm/bgb")));
+        PersistenceUri.set(properties, luceneTemporaryDirectory("bazza"));
         SearcherPoolActivator.setSearchPool(properties, LucenePool.class);
         Application application = new WebApplication(BasePath.basePath("/"), properties);
         new RestServer(
